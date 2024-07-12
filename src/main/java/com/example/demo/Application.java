80b839081eb274247fa617e9241e02082ec4fc1d
Manuel Haro <manuelharo84@gmail.com>
2024-07-11 22:10:23 -0500

    creación métodos saludo con pathVariable y requestParam

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,7 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @SpringBootApplication
 @RestController
@@ -13,6 +17,19 @@ public class Application {
   public String home() {
     return "Hello Docker World";
   }
+
+  @GetMapping("/saludo/{name}/{lastName}")
+  public String saludo(@PathVariable("name") String name, @PathVariable("lastName") String lastName){
+    return "Bienvenido estimado "+lastName+" "+name;
+  }
+
+  @GetMapping("/saludo")
+  public String saludo(@RequestParam("name") String name, @RequestParam("age") int age){
+    return "Hola "+name+" tienes "+age+" años";
+  }
+
+
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
